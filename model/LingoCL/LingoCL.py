c44cf9d45c7c8939d671e0c8feb51d1b74704a86
jackwang <jack4shihong@gmail.com>
2024-05-08 02:09:25 +0800

    moved

--- model/LingoCL/LingoCL.py
+++ model/LingoCL/LingoCL.py
@@ -1,4 +1,5 @@
 # Standard Library
+import copy
 from contextlib import contextmanager
 
 # Third-Party Library
@@ -8,6 +9,82 @@ import clip
 import torch
 import torch.nn as nn
 import torchvision.models as models
+
+# My Library
+from .iCaRL import iCaRL
+from utils.annotation import Task
+
+
+class iCaRL_LingoCL(iCaRL):
+
+    def __init__(self, feature_dim: int = 512) -> None:
+        super().__init__(feature_dim)
+
+        self.clip, _ = clip.load("ViT-B/32", device="cpu")
+
+    @contextmanager
+    def set_new_task(self, task: Task):
+        num_cls = len(task)
+        self.current_task = task
+
+        # before the start learning the task, expand the weight vectors
+
+        # create new weight vectors
+        self.current_weight_vectors = nn.Linear(
+            in_features=self.feature_dim, out_features=num_cls + len(self.learned_classes), bias=False).to(device=self.feature_extractor.conv1.weight.device)
+
+        # for the second task, copy the weights of last weight vectors
+        if len(self.weight_vectors) > 0:
+
+            # copy the weight of previous weight vector
+            previous_weight_vector = self.weight_vectors[-1]
+
+            self.current_weight_vectors.weight.data[:len(
+                self.learned_classes), :] = previous_weight_vector.weight.data[:, :]
+
+        # use clip to generate weights for new classes
+        text = clip.tokenize([f"a {i}" for i in task]).to(
+            self.feature_extractor.conv1.weight.device)
+
+        text_features: torch.FloatTensor
+        text_features = self.clip.encode_text(text)
+        text_features = text_features / \
+            text_features.norm(dim=1, keepdim=True)
+
+        # get weights for new classes
+        # Ref: Page 3, Section 3.2 Our Proposed Language-Guided Supervision, (ii)
+        weights = text_features
+
+        self.current_weight_vectors.weight.data[len(
+            self.learned_classes):] = weights
+
+        # freeze the weight vector
+        for param in self.current_weight_vectors.parameters():
+            param.requires_grad = False
+
+        # return to task learning
+        yield self
+
+        # after the task, save the feature extractor and weight vectors
+
+        # switch the current weight vectors to evaluation mode
+        self.current_weight_vectors.eval()
+
+        # save the current weight vectors
+        self.weight_vectors.append(self.current_weight_vectors)
+
+        # copy the last feature extractor
+        self.previous_feature_extractor = copy.deepcopy(
+            self.feature_extractor)
+
+        # freeze previous feature extractor and switch to evaluation mode
+        for param in self.previous_feature_extractor.parameters():
+            param.requires_grad = False
+
+        self.previous_feature_extractor.eval()
+
+        # expand the learned classes
+        self.learned_classes.extend(task)
 
 
 class CosineClassifier(nn.Module):
@@ -91,13 +168,20 @@ class LingoCL(nn.Module):
 
 
 if __name__ == "__main__":
-    lingo = LingoCL()
+    lingo = iCaRL_LingoCL()
 
     tasks = [["1", "2"], ["3", "4"]]
 
+    image = torch.randn(32, 3, 32, 32)
+
     for task in tasks:
         with lingo.set_new_task(task):
+            logits = lingo(image)
 
-            lingo.eval()
-            for param in lingo.current_classifier.parameters():
-                print(f"{param.requires_grad}")
+            print(logits.shape)
+
+        for cls_name in task:
+            lingo.exemplar_means[cls_name] = torch.randn(1, 512)
+
+        preds = lingo.get_preds(image)
+        print(preds)
