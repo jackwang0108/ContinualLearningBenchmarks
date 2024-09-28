5cbeeaa4373e0f160c8730425e51462699e95b29
jackwang <jack4shihong@gmail.com>
2024-09-28 20:10:42 +0800

    reformatted

--- model/finetune.py
+++ model/finetune.py
@@ -1,4 +1,5 @@
 # Standard Library
+import copy
 from contextlib import contextmanager
 
 # Third-Party Library
@@ -15,47 +16,74 @@ from .base import ContinualLearningModel
 
 class Finetune(nn.Module, ContinualLearningModel):
 
-    def __init__(self) -> None:
+    def __init__(self, backbone: nn.Module) -> None:
         super().__init__()
-        self.feature_extractor = models.resnet34(
-            weights=models.ResNet34_Weights.DEFAULT)
 
-        self.feature_dim = self.feature_extractor.fc.in_features
-        self.feature_extractor.fc = nn.Identity()
+        # set in __init__.py:get_backbone()
+        self.feature_dim = backbone.feature_dim
 
-        self.classifiers: list[nn.Linear] = []
-        self.current_classifier: nn.Linear = None
+        self.feature_extractor = backbone
+        self.previous_feature_extractors: list[nn.Module] = []
+
+        self.classifier: nn.Linear = None
+        self.previous_classifiers: list[nn.Linear] = []
+
+        self.previous_nets: list[nn.Sequential] = []
 
         self.current_task: Task = None
         self.learned_classes: list[str] = []
+        self.learned_tasks: list[Task] = []
 
     @contextmanager
     def set_new_task(self, task: list[str]):
         num_cls = len(task)
         self.current_task = task
 
-        self.current_classifier = nn.Linear(
-            in_features=self.feature_dim, out_features=num_cls + len(self.learned_classes)).to(device=self.feature_extractor.conv1.weight.device)
+        self.classifier = nn.Linear(
+            in_features=self.feature_dim,
+            out_features=num_cls + len(self.learned_classes),
+        ).to(device=next(self.feature_extractor.parameters()).device)
 
-        if len(self.classifiers) != 0:
-            previous_classifier = self.classifiers[-1]
+        if len(self.learned_tasks) > 0:
+            previous_classifier = self.previous_classifiers[-1]
 
-            self.current_classifier.weight.data[:len(
-                self.learned_classes), :] = previous_classifier.weight.data[:, :]
-
-        self.classifiers.append(self.current_classifier)
+            self.classifier.weight.data[: len(self.learned_classes), :] = (
+                previous_classifier.weight.data[:, :]
+            )
 
         # return to task learning
         yield self
 
+        # after the task, save the feature extractor and classifiers
+        self.previous_classifiers.append(copy.deepcopy(self.classifier))
+        self.previous_feature_extractors.append(copy.deepcopy(self.feature_extractor))
+
+        # freeze the current weight vector and switch to evaluation mode
+        self.previous_classifiers[-1].eval()
+        for param in self.previous_classifiers[-1].parameters():
+            param.requires_grad = False
+
+        # freeze previous feature extractor and switch to evaluation mode
+        self.previous_feature_extractors[-1].eval()
+        for param in self.previous_feature_extractors[-1].parameters():
+            param.requires_grad = False
+
+        self.previous_nets.append(
+            nn.Sequential(
+                self.previous_feature_extractors[-1],
+                self.previous_classifiers[-1],
+            )
+        )
+
         # expand the learned classes
         self.learned_classes.extend(task)
+        self.learned_tasks.append(task)
 
     def forward(self, image: torch.FloatTensor) -> torch.FloatTensor:
         # sourcery skip: inline-immediately-returned-variable
         features: torch.FloatTensor
         features = self.feature_extractor(image)
-        logits = self.current_classifier(features)
+        logits = self.classifier(features)
         return logits
 
 
--- src/finetune.py
+++ src/finetune.py
@@ -1,58 +1,102 @@
 # Standard Library
-import datetime
+import argparse
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
+from argparse import Namespace
 
 # Third-Party Library
 import numpy as np
+from loguru._logger import Logger
 
 # Torch Library
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
+from torch.optim import Optimizer
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 # My Library
 from model.finetune import Finetune
-from utils.helper import get_logger
-from utils.datasets import CLDatasetGetter
-from utils.helper import (plot_matrix, draw_image)
-from utils.helper import (get_probas, get_pred, to_khot)
-from utils.helper import (get_top1_acc,
-                          get_backward_transfer,
-                          get_last_setp_accuracy,
-                          get_average_incremental_accuracy,
-                          get_forgetting_rate,
-                          CLMetrics)
+from utils.helper import to_khot, get_top1_acc
 from utils.annotation import (
+    Task,
+    Images,
+    Labels,
     TaskLearner,
     PerformanceFunc,
-    CLAbilityTester,
 )
 
-rname = input("the name of this running: ")
-wname = f"{rname}-{datetime.datetime.now().strftime('%m-%d %H.%M')}"
-writer = SummaryWriter(log_dir := f"log/{wname}")
-logger = get_logger(Path(log_dir) / "running.log")
-device = torch.device("cuda:0")
-
-hparams_dict = {}
+device: torch.device = None
 
 
-def train_epoch(model: Finetune, train_loader: DataLoader, loss_func: nn.Module, optimizer: optim.Optimizer) -> torch.FloatTensor:
+def get_model(backbone: nn.Module) -> Finetune:
+    return Finetune(backbone)
+
+
+def get_args(argument_list: list[str]) -> tuple[Namespace, list[str]]:
+    parser = argparse.ArgumentParser(description="Train Finetune")
+
+    parser.add_argument(
+        "--optim",
+        type=str,
+        default="SGD",
+        choices=["Adam", "SGD"],
+        help="which optimizer to use",
+    )
+    model_args, unknow_args = parser.parse_known_args(argument_list)
+
+    return model_args, unknow_args
+
+
+@torch.no_grad()
+def get_preds(cl_model: Finetune, images: torch.FloatTensor) -> torch.LongTensor:
+    logits: torch.FloatTensor = cl_model(images)
+    probas = logits.softmax(dim=-1)
+    preds = probas.argmax(dim=-1)
+    return preds
+
+
+def prepare_continual_learning(module_args: Namespace, **kwargs):
+    return None
+
+
+def prepare_new_task(
+    module_args: Namespace,
+    **kwargs: dict[str, Any],
+):
+    return None
+
+
+def finish_new_task(
+    module_args: Namespace,
+    **kwargs: dict[str, Any],
+):
+    return None
+
+
+def train_epoch(
+    cl_model: Finetune,
+    train_loader: DataLoader,
+    optimizer: optim.Optimizer,
+) -> torch.FloatTensor:
     total_loss = 0
 
-    total_num_classes = len(model.learned_classes) + len(model.current_task)
+    loss_func = nn.CrossEntropyLoss()
+
+    num_new_classes = len(cl_model.current_task)
+    num_learned_classes = len(cl_model.learned_classes)
+    total_num_classes = num_new_classes + num_learned_classes
 
     loss: torch.FloatTensor
-    image: torch.FloatTensor
     label: torch.LongTensor
-    for image, label in train_loader:
-        image = image.to(device)
-        label = to_khot(label, total_num_classes).to(device)
+    original_image: torch.FloatTensor
+    augmented_image: torch.FloatTensor
+    for augmented_image, original_image, label in train_loader:
+        augmented_image = augmented_image.to(device)
+        label = label.to(device)
 
-        logits = model(image)
+        logits = cl_model(augmented_image)
         loss = loss_func(logits, label)
 
         total_loss += loss.clone().detach()
@@ -65,52 +109,87 @@ def train_epoch(model: Finetune, train_loader: DataLoader, loss_func: nn.Module,
 
 
 @torch.no_grad()
-def test_epoch(model: Finetune, test_loader: DataLoader, perf_func: PerformanceFunc, num_cls_per_task: int) -> tuple[torch.FloatTensor, torch.FloatTensor]:
+def test_epoch(
+    cl_model: Finetune,
+    test_loader: DataLoader,
+    perf_func: PerformanceFunc,
+    num_total_class: int,
+) -> tuple[torch.FloatTensor, torch.FloatTensor]:
     performance = []
 
-    for image, label in test_loader:
-        image, label = image.to(device), label.to(device)
-        logits = model(image)
-        probas = get_probas(logits)
-        preds = get_pred(probas)
-        performance.append(
-            perf_func(preds, label, len(model.learned_classes) + num_cls_per_task))
+    label: torch.LongTensor
+    original_image: torch.FloatTensor
+    augmented_image: torch.FloatTensor
+    for augmented_image, original_image, label in test_loader:
+        augmented_image, label = augmented_image.to(device), label.to(device)
+        preds = get_preds(cl_model, augmented_image)
+        performance.append(perf_func(preds, label, num_total_class))
 
     return sum(performance) / len(performance)
 
 
-def get_task_learner() -> TaskLearner:
+def get_task_learner(
+    main_args: Namespace, module_args: Namespace, **kwargs: dict[str, Any]
+) -> TaskLearner:
+
     num_task_learned = 0
 
-    def task_learner(task_id: int, num_cls_per_task: int, model: Finetune, train_loader: DataLoader, test_loader: DataLoader) -> Finetune:
+    def task_learner(**kwargs: dict[str, Any]) -> nn.Module:
+        global device
+        nonlocal main_args, module_args, num_task_learned
 
-        loss_func = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+        task_id: int = kwargs["task_id"]
+        current_task: Task = kwargs["current_task"]
+        cl_model: Finetune = kwargs["cl_model"]
+        train_loader: DataLoader = kwargs["train_loader"]
+        test_loader: DataLoader = kwargs["test_loader"]
+        logger: Logger = kwargs["logger"]
+        writer: SummaryWriter = kwargs["writer"]
+        hparams_dict: dict = kwargs["hparams_dict"]
+        training_watcher = kwargs["training_watcher"]
 
-        log_times = 5
-        num_epoch = 70
+        device = next(cl_model.parameters()).device
+
+        optimizer: Optimizer = getattr(optim, module_args.optim)(
+            cl_model.parameters(),
+            lr=float(main_args.lr),
+            weight_decay=1e-5,
+            # momentum=0.9,
+        )
+
+        num_epoch = main_args.epochs
+
         for epoch in range(num_epoch):
-            train_loss = train_epoch(model, train_loader, loss_func, optimizer)
-            test_top1_acc = test_epoch(model, test_loader, get_top1_acc,
-                                       num_cls_per_task)
 
-            if (epoch + 1) % (num_epoch // log_times) == 0:
+            train_loss = train_epoch(cl_model, train_loader, optimizer)
+
+            test_top1_acc = test_epoch(
+                cl_model,
+                test_loader,
+                get_top1_acc,
+                len(cl_model.learned_classes) + len(current_task),
+            )
+
+            # log epoch
+            print_interval = num_epoch // main_args.log_times
+            if (epoch + 1) % (print_interval if print_interval != 0 else 1) == 0:
                 logger.info(
-                    f"\tEpoch [{num_epoch}/{epoch+1:>{len(str(num_epoch))}d}], {train_loss=:.2f}, {test_top1_acc=:.2f}")
+                    f"\tEpoch [{num_epoch}/{epoch+1:>{len(str(num_epoch))}d}], {train_loss=:.3f}, {test_top1_acc=:.2f}"
+                )
 
-            # log training
-            training_watcher = {
-                "Train Loss": train_loss,
-                "Test Top1 Acc": test_top1_acc,
-            }
+            # log global
+            training_watcher["Train Loss"] = train_loss
+            training_watcher["Test Top1 Acc"] = test_top1_acc
 
             # watch training
             for watcher_name, watcher_value in training_watcher.items():
-                writer.add_scalar(f"Task Learning/{watcher_name}",
-                                  scalar_value=watcher_value, global_step=epoch + task_id * num_epoch)
+                writer.add_scalar(
+                    f"Task Learning/{watcher_name}",
+                    scalar_value=watcher_value,
+                    global_step=epoch + task_id * num_epoch,
+                )
 
         # log hparams
-        nonlocal num_task_learned
         if num_task_learned == 0:
             hparams_dict["num_epoch"] = num_epoch
             hparams_dict["optim"] = optimizer.__class__.__name__
@@ -123,123 +202,6 @@ def get_task_learner() -> TaskLearner:
                 hparams_dict["dampening"] = d
         num_task_learned += 1
 
-        return model
+        return cl_model
+
     return task_learner
-
-
-def get_continual_learning_ability_tester(task_num: int, num_cls_per_task: int) -> CLAbilityTester:
-
-    num_cls_per_task = num_cls_per_task
-    cl_matrix = np.zeros((task_num, task_num))
-
-    metrics_getter = CLMetrics({
-        "Backward Transfer": get_backward_transfer,
-        "Forgetting Rate": get_forgetting_rate,
-        "Last Step Accuracy": get_last_setp_accuracy,
-        "Average Incremental Accuracy": get_average_incremental_accuracy,
-    })
-
-    @torch.no_grad
-    def continual_learning_ability_tester(task_id: int, learned_tasks: list[list[str]], learned_task_loaders: list[DataLoader], model: Finetune) -> tuple[np.ndarray, Optional[dict[str, float]]]:
-        nonlocal cl_matrix, num_cls_per_task
-
-        # test on all tasks, including previous and current task
-        for i, previous_loader in enumerate(learned_task_loaders):
-
-            cl_matrix[i, task_id] = test_epoch(
-                model, previous_loader, get_top1_acc, num_cls_per_task)
-
-            logger.info(
-                f"\ttest on task {i}, test_acc={cl_matrix[i, task_id]: .2f}, {learned_tasks[i]}")
-
-        # calculate continual learning ability metrics and log to summarywriter
-        if task_id >= 1:
-            current_cl_matrix = cl_matrix[:task_id+1, :task_id+1]
-
-            current_metrics = metrics_getter(current_cl_matrix, "mean")
-
-            # log to summarywriter
-            for metric_name, metric_value in current_metrics.items():
-                if isinstance(metric_value, (float, int)):
-                    writer.add_scalar(
-                        tag=f"Continual Learning Metrics/{metric_name}",
-                        scalar_value=metric_value,
-                        global_step=task_id
-                    )
-
-        # draw heatmap of cl_matrix and log to summarywriter
-        writer.add_figure(
-            tag=f"cl_matrix/{wname}",
-            figure=plot_matrix(cl_matrix, task_id),
-            global_step=task_id,
-        )
-
-        return cl_matrix, current_metrics if task_id >= 1 else None
-
-    return continual_learning_ability_tester
-
-
-def continual_learning():
-
-    dataset_getter = CLDatasetGetter(
-        dataset="cifar100", task_num=10, fixed_task=True)
-
-    model = Finetune().to(device)
-
-    # logging
-    logger.info(f"Model: {model.__class__.__name__}")
-    logger.info("Task List:")
-    for task_id, task in enumerate(dataset_getter.tasks):
-        logger.info(f"\tTask {task_id}: {task}")
-
-    # get task learner and cl-ability tester
-    task_learner = get_task_learner()
-
-    continual_learning_ability_tester = get_continual_learning_ability_tester(
-        dataset_getter.task_num, dataset_getter.num_cls_per_task)
-
-    learned_tasks: list[DataLoader] = []
-    learned_task_loaders: list[DataLoader] = []
-    for task_id, current_task, train_dataset, test_dataset in dataset_getter:
-        # prepare the data for the task
-        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
-
-        # learn the new task
-        with model.set_new_task(current_task):
-
-            logger.success(f"{task_id=}, {current_task=}")
-            model = task_learner(
-                task_id, dataset_getter.num_cls_per_task, model, train_loader, test_loader)
-
-        # save the test loader for continual learning testing
-        learned_tasks.append(current_task)
-        learned_task_loaders.append(test_loader)
-
-        # test continual learning performance
-        cl_matrix, metrics = continual_learning_ability_tester(
-            task_id, learned_tasks, learned_task_loaders, model)
-
-        if metrics is not None:
-
-            logger.debug(
-                "\t " + ", ".join([f"{key}={value:.2f}" for key, value in metrics.items()]))
-
-    # writer.add_hparams(hparams_dict, metrics_dict)
-
-    # log hyper parameter
-    logger.info("Hyper Parameters")
-    for key, value in hparams_dict.items():
-        logger.info(f"\t{key}: {value}")
-
-    # log continual learning metrics
-    logger.debug("Continual Learning Performance:")
-    for key, value in metrics.items():
-        logger.info(f"\t{key}: {value}")
-
-    logger.info(f"Task learned: {dataset_getter.tasks}")
-    logger.success("Finished Training")
-
-
-if __name__ == "__main__":
-    continual_learning()
