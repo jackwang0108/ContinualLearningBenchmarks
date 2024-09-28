449d3ad8f5021d3a32256be57cf277d927e81774
jackwang <jack4shihong@gmail.com>
2024-09-28 18:00:46 +0800

    updated

--- model/iCaRL.py
+++ model/iCaRL.py
@@ -20,25 +20,28 @@ class iCaRL(nn.Module, ContinualLearningModel):
 
     def __init__(self, backbone: nn.Module) -> None:
         super().__init__()
-        
+
         self.feature_extractor = backbone
 
         # set in __init__.py:get_backbone()
         self.feature_dim = self.feature_extractor.feature_dim
 
         # after learning the current task, the mean of exemplar sets need to be recalculated, so we need to save the current feature extractor
-        self.previous_feature_extractor: ResNet = None
+        self.previous_feature_extractors: list[ResNet] = []
 
         # Note: iCaRL uses weight vectors for representation learning, not classification
         # Ref: Page 3, Architecture, Paragraph 2, Line 11: "Note that even though one can interpret these outputs as probabilities, iCaRL uses the network only for representation learning, not for the actual classification step."
-        self.weight_vectors: list[nn.Linear] = []
+        self.previous_weight_vectors: list[nn.Linear] = []
         self.current_weight_vectors: nn.Linear = None
+
+        self.previous_nets: list[nn.Sequential] = []
 
         # Note: iCaRL uses Nearest-Mean-of-Exemplars to classify a given example
         self.exemplar_means: dict[str, torch.FloatTensor] = {}
 
         self.current_task: Task = None
         self.learned_classes: list[str] = []
+        self.learned_tasks: list[Task] = []
 
     @contextmanager
     def set_new_task(self, task: Task):
@@ -49,52 +52,69 @@ class iCaRL(nn.Module, ContinualLearningModel):
 
         # create new weight vectors
         self.current_weight_vectors = nn.Linear(
-            in_features=self.feature_dim, out_features=num_cls + len(self.learned_classes), bias=False).to(device=self.feature_extractor.conv1.weight.device)
+            in_features=self.feature_dim,
+            out_features=num_cls + len(self.learned_classes),
+            bias=False,
+        ).to(device=self.feature_extractor.conv1.weight.device)
 
         # for the second task, copy the weights of last weight vectors
-        if len(self.weight_vectors) > 0:
+        if len(self.previous_weight_vectors) > 0:
 
             # copy the weight of previous weight vector
-            previous_weight_vector = self.weight_vectors[-1]
+            previous_weight_vector = self.previous_weight_vectors[-1]
 
-            self.current_weight_vectors.weight.data[:len(
-                self.learned_classes), :] = previous_weight_vector.weight.data[:, :]
+            self.current_weight_vectors.weight.data[: len(self.learned_classes), :] = (
+                previous_weight_vector.weight.data[:, :]
+            )
 
         # return to task learning
         yield self
 
         # after the task, save the feature extractor and weight vectors
+        self.previous_weight_vectors.append(copy.deepcopy(self.current_weight_vectors))
+        self.previous_feature_extractors.append(copy.deepcopy(self.feature_extractor))
 
         # freeze the current weight vector and switch to evaluation mode
-        for param in self.current_weight_vectors.parameters():
+        self.previous_weight_vectors[-1].eval()
+        for param in self.previous_feature_extractors[-1].parameters():
             param.requires_grad = False
-
-        self.current_weight_vectors.eval()
-
-        # save the current weight vectors
-        self.weight_vectors.append(self.current_weight_vectors)
-
-        # copy the last feature extractor
-        self.previous_feature_extractor = copy.deepcopy(
-            self.feature_extractor)
 
         # freeze previous feature extractor and switch to evaluation mode
-        for param in self.previous_feature_extractor.parameters():
+        self.previous_feature_extractors[-1].eval()
+        for param in self.previous_feature_extractors[-1].parameters():
             param.requires_grad = False
 
-        self.previous_feature_extractor.eval()
+        self.previous_nets.append(
+            nn.Sequential(
+                self.previous_feature_extractors[-1],
+                self.previous_weight_vectors[-1],
+            )
+        )
 
         # expand the learned classes
         self.learned_classes.extend(task)
+        self.learned_tasks.append(task)
 
     @contextmanager
-    def use_previous_model(self, feature_extractor: Optional[ResNet] = None, weight_vectors: Optional[nn.Linear] = None):
+    def use_previous_model(
+        self,
+        feature_extractor: Optional[ResNet] = None,
+        weight_vectors: Optional[nn.Linear] = None,
+    ):
         current_feature_extractor = self.feature_extractor
         current_weight_vectors = self.current_weight_vectors
 
         try:
-            self.feature_extractor = feature_extractor if feature_extractor is not None else self.previous_feature_extractor
-            self.current_weight_vectors = weight_vectors if weight_vectors is not None else self.weight_vectors[-1]
+            self.feature_extractor = (
+                feature_extractor
+                if feature_extractor is not None
+                else self.previous_feature_extractors
+            )
+            self.current_weight_vectors = (
+                weight_vectors
+                if weight_vectors is not None
+                else self.previous_weight_vectors[-1]
+            )
             yield self
         finally:
             self.feature_extractor = current_feature_extractor
@@ -140,7 +160,8 @@ class iCaRL(nn.Module, ContinualLearningModel):
 
         # [1, N, 512]
         cls_means: torch.FloatTensor = torch.cat(
-            list(self.exemplar_means.values()), dim=0).unsqueeze(dim=0)
+            list(self.exemplar_means.values()), dim=0
+        ).unsqueeze(dim=0)
 
         # [B, N, 512]
         pred = features - cls_means
@@ -164,7 +185,6 @@ if __name__ == "__main__":
 
         # calculate mean of class
         for i, t_name in enumerate(t):
-            model.exemplar_means[t_name] = (
-                torch.ones(1, 512) * i).to("cuda:0")
+            model.exemplar_means[t_name] = (torch.ones(1, 512) * i).to("cuda:0")
 
         pred = model.get_preds(test_image)
