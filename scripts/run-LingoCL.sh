66882aa944a893032436d7b53064d7d50133f606
jackwang <jack4shihong@gmail.com>
2024-09-29 17:10:02 +0800

    finished LingoCL

---
+++ scripts/run-LingoCL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+root_dir=$(realpath "$(dirname "$(realpath "$0")")/../")
+
+# check src/main.py for general args and src/LUCIR.py for algorithm args
+
+cl_algo="LingoCL"
+
+# running with paper hyper-parameters
+cd "$root_dir" && python -m main --model "${cl_algo}" --backbone "resnet34" --dataset "cifar100" --num_tasks 10 --lr 0.1 --epochs 160 --batch_size 128 --each_buffer_size 20 --log_times 10 --gpu_id 0 --name "NAME OF THE LOGGING DIR" --message "LEAVE YOUR COMMENT OF THIS RUNNING" 
+
+
+# passing user-defined templates for generating text embeddings
+cd "$root_dir" && python -m main --model "${cl_algo}" --backbone "resnet34" --dataset "cifar100" --num_tasks 10 --fixed_tasks --lr 0.1 --epochs 160 --batch_size 32 --each_buffer_size 20 --templates "a image of [CLS]" --log_times 10 --gpu_id 0 --name "NAME OF THE LOGGING DIR" --message "LEAVE YOUR COMMENT OF THIS RUNNING" 
+
+# use same buffer management policy and epochs for fair comparison
+cd "$root_dir" && python -m main --model "${cl_algo}" --backbone "resnet34" --dataset "cifar100" --num_tasks 10 --fixed_tasks --lr 0.1 --epochs 100 --batch_size 32 --total_buffer_size 2000 --log_times 10 --gpu_id 0 --name "NAME OF THE LOGGING DIR" --message "LEAVE YOUR COMMENT OF THIS RUNNING" 
